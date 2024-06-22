5dc1426841cd03d61823e4c3f161cd70f5e441c9
jo.quintero <jo.quintero@duocuc.cl>
2024-06-21 23:44:01 -0400

    actualizacion_1

--- inicio/views.py
+++ inicio/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
+from django.http import HttpResponse # type: ignore
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Esto Funciona!!")
