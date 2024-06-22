be555ace1db1c6e6377c367a1e05fc48a765591b
jo.quintero <jo.quintero@duocuc.cl>
2024-06-22 00:06:54 -0400

    actualizacion_4

--- inicio/urls.py
+++ inicio/urls.py
@@ -3,5 +3,13 @@ from django.urls import path # type: ignore
 from . import views
 
 urlpatterns = [
-    path("home", views.index),
+    path("index", views.index),
+    path("ubicacion", views.ubicacion),
+    path("servicios", views.servicios),
+    path("preguntas_frecuentes", views.preguntas_frecuentes),
+    path("contacto", views.contacto),
+    path("blog", views.blog),
+    path("donaciones", views.donaciones),
+    
+    
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -4,4 +4,22 @@ from django.http import HttpResponse # type: ignore
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Esto Funciona!!")
+    return HttpResponse("PAGINA DE INICIO!!")
+
+def ubicacion(request):
+    return HttpResponse("UBICACIÓN!!")
+
+def servicios(request):
+    return HttpResponse("SERVICIOS!!")
+
+def preguntas_frecuentes(request):
+    return HttpResponse("PREGUNTAS FRECUENTES!!")
+
+def contacto(request):
+    return HttpResponse("CONTACTO!!")
+
+def blog(request):
+    return HttpResponse("BLOG!!")
+
+def donaciones(request):
+    return HttpResponse("DONACIONES!!")
