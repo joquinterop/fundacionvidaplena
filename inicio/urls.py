8509ac2e734fd43a9c65480e1a8337280387c200
jo.quintero <jo.quintero@duocuc.cl>
2024-06-23 16:47:08 -0400

    actualizacion_22

--- inicio/urls.py
+++ inicio/urls.py
@@ -3,9 +3,9 @@ from django.urls import path # type: ignore
 from . import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
     path("home", views.home),
-    path("ubicacion", views.ubicacion),
+    path("ubicacion", views.ubicacion, name="ubicacion"),
     path("servicios", views.servicios),
     path("preguntas_frecuentes", views.preguntas_frecuentes),
     path("contacto", views.contacto),
