ce9e8697b8430af1a9290943978462c41dd2e17f
jo.quintero <jo.quintero@duocuc.cl>
2024-06-26 19:44:42 -0400

    act_65

--- inicio/urls.py
+++ inicio/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("home", views.home, name="home"),
-    path("ubicacion", views.ubicacion, name="ubicacion"),
+    path("/home", views.home, name="home"),
+    path("/ubicacion", views.ubicacion, name="ubicacion"),
     path("servicios", views.servicios),
     path("preguntas_frecuentes", views.preguntas_frecuentes),
     path("contacto", views.contacto),
