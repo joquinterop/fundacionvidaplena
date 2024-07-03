17721c22bff6af4007c2ae70d0852c2019212b30
jo.quintero <jo.quintero@duocuc.cl>
2024-07-03 02:16:48 -0400

    ACT_82

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,18 +1,7 @@
 from django.urls import path # type: ignore
 
 from . import views
-"""
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("home", views.inicio, name="inicio"),
-    path("ubicacion", views.ubicacion, name="ubicacion"),
-    path("servicios", views.servicios, name="servicios"),
-    path("preguntas_frecuentes", views.preguntas_frecuentes, name="preguntas_frecuentes"),
-    path("contacto", views.contacto, name="contacto"),
-    path("blog", views.blog, name="blog"),
-    path("donaciones", views.donaciones, name="donaciones"),
-]
-"""
+
 urlpatterns = [
     path("", views.index, name='index'),
     path('inicio/', views.inicio, name='inicio'),
